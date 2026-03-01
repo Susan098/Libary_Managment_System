fe4e0822ad7ccd2a9596be3eb4eddd6ee2611d0b
Susan Subedi <susan.subedi@example.com>
2026-03-01 08:49:35 +0545

    update

--- src/main/java/com/example/libary/Main.java
+++ src/main/java/com/example/libary/Main.java
@@ -1,7 +1,7 @@
 package com.example.library;
+
 import com.example.library.controller.LibraryController;
 import com.example.library.utils.InputUtils;
-
 public class Main {
     public static void main(String[] args) {
         LibraryMenu menu = new LibraryMenu();
--- src/main/java/com/example/libary/controller/LibraryMenu.java
+++ src/main/java/com/example/libary/controller/LibraryMenu.java
@@ -1,7 +1,4 @@
-package com.example.library;
 
-import com.example.library.controller.LibraryController;
-import com.example.library.utils.InputUtils;
 
 public class LibraryMenu {
     private LibraryController controller;
